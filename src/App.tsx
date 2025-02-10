3149918fff4dd5f686ce439b6c1e66bd86f8e841
André Felipe <andre1104mg@gmail.com>
2025-02-10 18:00:01 -0300

    github actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
 import Steve from "./Steve.tsx";
 import "./App.css";
 
 export default function App() {
-  return (
-    <div className="app">
-      <Steve />
-    </div>
-  );
+  return <Steve />;
 }
